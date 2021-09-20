237474399c515493321572c8fa839266ca520284
Peter Watts <pwattsmail@gmail.com>
2021-09-20 16:08:04 +1000

    revert

--- apps/web/graphqlClient.js
+++ apps/web/graphqlClient.js
@@ -1,13 +1,13 @@
 import { GraphQLClient } from "graphql-hooks";
 
 let config;
-if(process.env.CHAIN_ID=='1') {
+if(process.env.CHAIN_ID=='4') {
   config = {
-    url: "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2"
+    url: "https://api.studio.thegraph.com/query/8490/loot-exchange--rinkeby/v0.0.3"
   }
 } else {
   config = {
-    url: "https://api.studio.thegraph.com/query/8490/loot-exchange--rinkeby/v0.0.3"
+    url: "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2"
   }
 }
 const client = new GraphQLClient(config);
