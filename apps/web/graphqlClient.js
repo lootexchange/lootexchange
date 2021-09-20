df1d41c7ec9188edf57994eb2bc855bed1db9062
Peter Watts <pwattsmail@gmail.com>
2021-09-20 16:03:30 +1000

    different env, different subgraph

--- apps/web/graphqlClient.js
+++ apps/web/graphqlClient.js
@@ -1,8 +1,15 @@
 import { GraphQLClient } from "graphql-hooks";
 
-const client = new GraphQLClient({
-  // having trouble putting this into .env.local
-  url: "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2"
-});
+let config;
+if(process.env.CHAIN_ID=='1') {
+  config = {
+    url: "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2"
+  }
+} else {
+  config = {
+    url: "https://api.studio.thegraph.com/query/8490/loot-exchange--rinkeby/v0.0.3"
+  }
+}
+const client = new GraphQLClient(config);
 
 export default client;
