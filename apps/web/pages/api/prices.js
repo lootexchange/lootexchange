54d09edbbc7b0e2155abeafc7ca1dd5fc3f181a3
George Roman <george.roman.99@gmail.com>
2021-09-20 08:39:09 +0200

    Fix missing client-side env variables

--- apps/web/pages/api/prices.js
+++ apps/web/pages/api/prices.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 export default async (req, res) => {
   let response = await fetch(
-    `${process.env.API_BASE}/collection/${process.env.LOOT_CONTRACT}/prices`
+    `${process.env.NEXT_PUBLIC_API_BASE}/collection/${process.env.NEXT_PUBLIC_LOOT_CONTRACT}/prices`
   );
   let json = await response.json();
 
