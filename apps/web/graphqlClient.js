18d02a390830c31f1e79aa7a57bc99604cc870ea
George Roman <george.roman.99@gmail.com>
2021-09-20 13:49:30 +0200

    Fix wrong subgraph

--- apps/web/graphqlClient.js
+++ apps/web/graphqlClient.js
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-hooks";
 
 const config = {
   url:
-    process.env.NEXT_PUBLIC_CHAIN_ID === "4"
+    process.env.NEXT_PUBLIC_CHAIN_ID == "4"
       ? "https://api.studio.thegraph.com/query/8490/loot-exchange--rinkeby/v0.0.3"
       : "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2",
 };
