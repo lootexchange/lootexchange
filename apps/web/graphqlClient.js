01429f28716237270975af78756898f380f8a9aa
George Roman <george.roman.99@gmail.com>
2021-09-20 08:31:33 +0200

    Fix unavailable chain id env variable

--- apps/web/graphqlClient.js
+++ apps/web/graphqlClient.js
@@ -1,15 +1,11 @@
 import { GraphQLClient } from "graphql-hooks";
 
-let config;
-if(process.env.CHAIN_ID=='4') {
-  config = {
-    url: "https://api.studio.thegraph.com/query/8490/loot-exchange--rinkeby/v0.0.3"
-  }
-} else {
-  config = {
-    url: "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2"
-  }
-}
+const config = {
+  url:
+    process.env.NEXT_PUBLIC_CHAIN_ID === "4"
+      ? "https://api.studio.thegraph.com/query/8490/loot-exchange--rinkeby/v0.0.3"
+      : "https://api.studio.thegraph.com/query/8490/loot-market-lunar/0.0.2",
+};
 const client = new GraphQLClient(config);
 
 export default client;
