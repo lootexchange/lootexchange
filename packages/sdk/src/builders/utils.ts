cb17c42c14be82f98b3875eede9586e598ce1fc4
George Roman <george.roman.99@gmail.com>
2021-09-27 16:29:48 +0200

    Add support for dutch auctions and order cancelling

--- packages/sdk/src/builders/utils.ts
+++ packages/sdk/src/builders/utils.ts
@@ -1,5 +1,13 @@
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
 import { randomBytes } from "@ethersproject/random";
+
+export const getDutchAuctionSellTime = () =>
+  // Allow 2 minutes to cover any on-chain discrepancies
+  Math.floor(Date.now() / 1000) - 2 * 60;
+
+export const getDutchAuctionBuyTime = () =>
+  // Allow 2 minutes to cover any on-chain discrepancies
+  Math.floor(Date.now() / 1000) + 2 * 60;
 
 export const getListingTime = () =>
   // Set the listing time 5 minutes in the past to make sure
@@ -8,3 +16,5 @@ export const getListingTime = () =>
   Math.floor(Date.now() / 1000) - 5 * 60;
 
 export const getSalt = () => BigNumber.from(randomBytes(32));
+
+export const bn = (value: BigNumberish) => BigNumber.from(value);
