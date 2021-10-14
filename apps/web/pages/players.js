c4ca7454c787b5ae55ec24695524aa99ff9670dd
Michael Morrissey <michael@philsophie.is>
2021-10-14 19:27:36 -0400

    add synth loot to inventory

---
+++ apps/web/pages/players.js
@@ -0,0 +1,12 @@
+import { Flex } from "@ui";
+import Header from "@ui/organisms/Header";
+
+const Players = () => {
+  return (
+    <Flex flex={1} flexDirection="column" bg="background">
+      <Header />
+    </Flex>
+  );
+};
+
+export default Players;
